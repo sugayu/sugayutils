8718cc55b0abe920d73cd8e011e7ebd1c0f746a9
sugayu <sugayu@aoni.waseda.jp>
2024-01-17 17:04:34 +0100

    Change way to import modules by editing init.py

--- sugayutils/core/__init__.py
+++ sugayutils/core/__init__.py
@@ -1,18 +1,15 @@
 '''Core directory
 '''
-from . import hello
-from .hello import *
 from . import const
-from .const import *
+from .const import Colors, AstroConst, colors
 from . import literature
-from .literature import *
+from .literature import DustAttenuationLaw, IRXbeta
 from . import misc
-from .misc import *
+from .misc import scale, get_nearest, get_argnearest, stat
 from . import line
-from .line import *
+from .line import LineWavelengthAt, LineList
 
-__all__ = hello.__all__
-__all__ += const.__all__
+__all__ = const.__all__
 __all__ += literature.__all__
 __all__ += misc.__all__
 __all__ += line.__all__
